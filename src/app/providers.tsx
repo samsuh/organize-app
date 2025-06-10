7e68d027d805b1846bb91baef7e9c34ce7fb9792
Sam Suh <samsuh@gmail.com>
2025-06-10 22:55:54 +0900

    wired up nextui. this is the old version before changed to heroui. refactor later

---
+++ src/app/providers.tsx
@@ -0,0 +1,11 @@
+'use cleint'
+
+import { NextUIProvider } from '@nextui-org/react'
+
+interface ProvidersProps {
+  children: React.ReactNode
+}
+
+export default function Providers({ children }: ProvidersProps) {
+  return <NextUIProvider>{children}</NextUIProvider>
+}
