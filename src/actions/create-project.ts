ded438a36ddfaddc72cf768fef3ddb456ffc7952
Samuel Suh <samsuh@gmail.com>
2025-06-16 20:06:41 +0900

    thought through revalidation locations for each server action and marked it for later

--- src/actions/create-project.ts
+++ src/actions/create-project.ts
@@ -1,2 +1,4 @@
 'use server'
-export async function createProject() {}
+export async function createProject() {
+  //todo: revalidate homepage
+}
--- src/actions/create-task.ts
+++ src/actions/create-task.ts
@@ -1,2 +1,4 @@
 'use server'
-export async function createTask() {}
+export async function createTask() {
+  //todo: revalidate project show page
+}
