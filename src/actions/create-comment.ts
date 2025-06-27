29a83a2bef5486df14d3cf157f3f1c9bcb2aabd0
Sam Suh <samsuh@gmail.com>
2025-06-27 17:23:23 +0900

    add next.config.ts image support, and create-comment server action and basic structure for taskshowpage

--- src/actions/create-comment.ts
+++ src/actions/create-comment.ts
@@ -1,4 +1,88 @@
 'use server'
-export async function createComment() {
-    //todo: revalidate task show page
+
+import { auth } from '@/auth'
+import { db } from '@/db'
+import paths from '@/paths'
+import { revalidatePath } from 'next/cache'
+import { z } from 'zod'
+
+const createCommentSchema = z.object({
+  content: z.string().min(3),
+})
+
+interface CreateCommentFormState {
+  errors: {
+    content?: string[]
+    _form?: string[]
+  }
+  success?: boolean
+}
+
+export async function createComment(
+  { taskId, parentId }: { taskId: string; parentId?: string },
+  formState: CreateCommentFormState,
+  formData: FormData
+): Promise<CreateCommentFormState> {
+  const result = createCommentSchema.safeParse({
+    content: formData.get('content'),
+  })
+
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    }
+  }
+
+  const session = await auth()
+  if (!session || !session.user || !session.user.id) {
+    return {
+      errors: {
+        _form: ['You must sign in to do this'],
+      },
+    }
+  }
+
+  try {
+    await db.comment.create({
+      data: {
+        content: result.data.content,
+        taskId: taskId,
+        parentId: parentId,
+        userId: session.user.id,
+      },
+    })
+  } catch (error) {
+    if (error instanceof Error) {
+      return {
+        errors: {
+          _form: [error.message],
+        },
+      }
+    } else {
+      return {
+        errors: {
+          _form: ['Something went wrong'],
+        },
+      }
+    }
+  }
+
+  const project = await db.project.findFirst({
+    where: { tasks: { some: { id: taskId } } },
+  })
+
+  if (!project) {
+    return {
+      errors: {
+        _form: ['Failed to revalidate project'],
+      },
+    }
+  }
+
+  //revalidate task show page
+  revalidatePath(paths.taskShow(project.slug, taskId))
+  return {
+    errors: {},
+    success: true,
+  }
 }
--- src/app/projects/[slug]/tasks/[taskId]/page.tsx
+++ src/app/projects/[slug]/tasks/[taskId]/page.tsx
@@ -1,7 +1,24 @@
-export default async function TaskShowPage() {
+import paths from '@/paths'
+import Link from 'next/link'
+
+interface TaskShowPageProps {
+  params: Promise<{
+    slug: string
+    taskId: string
+  }>
+}
+
+export default async function TaskShowPage({ params }: TaskShowPageProps) {
+  const { slug, taskId } = await params
+
   return (
-    <div>
-      <h3>Task Show Page</h3>
+    <div className='space-y-3'>
+      <Link
+        className='underline decoration-solid'
+        href={paths.projectShow(slug)}
+      >
+        {'< '} Back to {slug}
+      </Link>
     </div>
   )
 }
